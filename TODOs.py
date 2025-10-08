fa0206ade8a9e2594e6e282c9f38afa5770c5f7e
Thomas Stähli <staehtho@students.zhaw.ch>
2025-10-08 15:40:19 +0200

    general TODO

---
+++ TODOs.py
@@ -0,0 +1,7 @@
+# TODO: Jede Funktion mit einem Docstring im Google-Style ergänzen (ChatGPT)
+# TODO: Gütekriterium ITEA in MATLAB implementieren
+# TODO: Gütekriterium ITEA in Python implementieren und mit MATLAB verifizieren
+# TODO: Weitere Gütekriterien in Python implementieren und mit MATLAB verifizieren
+# TODO: Für PSO die zu minimierende Funktion erstellen
+# TODO: Thomas erklärt Flo MatlabInterface :)
+# TODO: Thomas zeigt Forschungsziel, Forschungsfragen und Methoden (SWA)
