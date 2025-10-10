5f5fee9936fb153cb6ebf2c8876c13c1982185b7
flo <buechfl2@students.zhaw.ch>
2025-10-10 22:12:55 +0200

    added documents

--- src/TestFiles/test_plant.py
+++ src/TestFiles/test_plant.py
@@ -9,7 +9,7 @@ def main():
     den = [1, 3, 2]
     plant: Plant = Plant(num, den)
 
-    pid: PIDClosedLoop = PIDClosedLoop(plant, kp=4.92, tn=19.2e-3, tv=5.7e-3, derivative_filter_ratio=0.001)
+    pid: PIDClosedLoop = PIDClosedLoop(plant, kp=4.92, tn=19.2e-3, tv=5.7e-3, derivative_filter_ratio=0.01)
 
     with MatlabInterface() as mat:
         s = "tf('s');"
@@ -23,15 +23,17 @@ def main():
         #    "Matlab": (omega, mag, phase),
         # })
         Kp = pid.kp
-        Td = pid.tn
+        Td = pid.tv
+        #Ti = pid.tv
         Ti = pid.tn
-        F = f"s / (0.01 * {plant.t1} * s + 1);"
+        #F = f"s / (0.01 * {plant.t1} * s + 1);"
+        F = f"s / (0.001 * s + 1);"
         mat.write_in_workspace(Kp=Kp, Td=Td, Ti=Ti, F=F)
         mat.run_simulation("model", "yout", stop_time=10)
         mat.plot_simulation("1", "Test", show=False)
 
         # t, y = plant.step_response(method="RK4")
-        t_cl, y_cl = pid.step_response(t0=1, method="RK4")
+        t_cl, y_cl = pid.step_response(t0=1, method="RK23")
         plt.plot(t_cl, y_cl, label="python cl")
         plt.legend()
         # plt.plot(t, y)
