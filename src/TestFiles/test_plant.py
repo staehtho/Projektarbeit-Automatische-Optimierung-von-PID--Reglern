ec15b73e91cffca8b2020db5186ef2e9331d9420
Thomas Stähli <staehtho@students.zhaw.ch>
2025-10-12 20:04:21 +0200

    test ITEA

--- src/TestFiles/test_plant.py
+++ src/TestFiles/test_plant.py
@@ -1,15 +1,15 @@
 import numpy as np
-from src.controlsys import Plant, PIDClosedLoop, bode_plot
+from src.controlsys import Plant, PIDClosedLoop, bode_plot, itae
 from src.Matlab import MatlabInterface
 import matplotlib.pyplot as plt
 
 
 def main():
-    num = [1, 2]
-    den = [1, 10, 2]
+    num = [1]
+    den = [1, 0.6, 1]
     plant: Plant = Plant(num, den)
 
-    pid: PIDClosedLoop = PIDClosedLoop(plant, Kp=4.92, Ti=19.2e-3, Td=5.7e-3, derivative_filter_ratio=0.01)
+    pid: PIDClosedLoop = PIDClosedLoop(plant, Kp=10, Ti=3, Td=0.8, derivative_filter_ratio=0.01)
 
     with MatlabInterface() as mat:
         s = "tf('s');"
@@ -27,11 +27,13 @@ def main():
         Ti = pid.Ti
         F = f"s / ({pid.Tf} * s + 1);"
         mat.write_in_workspace(Kp=Kp, Td=Td, Ti=Ti, F=F)
-        mat.run_simulation("model", "yout", stop_time=10, max_step=0.001)
+        mat.run_simulation("model", "yout", stop_time=20, max_step=0.001)
         mat.plot_simulation("1", "Test", show=False)
+        print(f"Matlab ITEA: {itae(mat.t, mat.values['value_y']['value'], 1)}")
 
         # t, y = plant.step_response(method="RK4")
-        t_cl, y_cl = pid.step_response(t0=1, dt=0.001, method="RK4")
+        t_cl, y_cl = pid.step_response(t0=0, t1=20, dt=0.001, method="RK23")
+        print(f"Python ITEA: {itae(t_cl, y_cl, 1)}")
         plt.plot(t_cl, y_cl, label="python cl")
         plt.legend()
         # plt.plot(t, y)
