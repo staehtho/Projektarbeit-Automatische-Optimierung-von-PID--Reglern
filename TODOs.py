a1b2ed119215c9e0e4b0cc5a52b5ee87f3142e13
Thomas Stähli <staehtho@students.zhaw.ch>
2025-10-27 14:24:32 +0100

    ToDo

--- TODOs.py
+++ TODOs.py
@@ -4,3 +4,6 @@
 # TODO: Weitere Gütekriterien in Python implementieren und mit MATLAB verifizieren
 # TODO: Für PSO die zu minimierende Funktion erstellen
 # TODO: Performance des Solvers mit verschiedenen Methoden untersuchen
+# TODO: PT4 mit Matlab vergleichen
+# TODO: ITEA berechnen, von publizierten Kp, Ti, Td und vergleichen in Matlab und Python
+# TODO: Überschwingendes System D > 1
