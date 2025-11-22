5fca26d3d10e74d54da13c763275babf2f470a20
flo <buechfl2@students.zhaw.ch>
2025-11-22 21:34:22 +0100

    todos aufgeräumt

--- TODOs.py
+++ TODOs.py
@@ -3,15 +3,11 @@
 # TODO: PID (PSO) finden und Voicecoil
 # TODO: Voicecoil regeln und aufzeichnen
 
-# TODO: Ausgabe als PDF verschönern (Flo)
 # TODO: Namen hinterlegen im code(main) (Flo)
-# TODO: dynamische integrationszeit, anhand sprungantwort plant (yaml: bandgrösse in %, endtime: auto oder wert (flo)
 
 # TODO: vorbereiten alternative erregerfunktion (Thomas)
 # TODO: contional windup umsetzen oder rückbauen (Thomas)
-# TODO: IAE ergänzen (thomas)
 # TODO: Alternativer solver vorbereiten (thomas)
 
-# TODO: EXE erstellen und an Büchi senden (thomas)
 
 # TODO: Stichworte zu BA, was solle diese beinhalten
