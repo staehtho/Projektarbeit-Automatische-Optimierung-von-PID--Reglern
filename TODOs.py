239135b90aa111297f3786091a6da2dd7cee9d49
flo <buechfl2@students.zhaw.ch>
2025-12-02 21:59:42 +0100

    report generator

--- TODOs.py
+++ TODOs.py
@@ -1,14 +1,7 @@
-# TODO: Jede Funktion mit einem Docstring im Google-Style ergänzen (ChatGPT)
-# TODO: Namen hinterlegen im code(main) (Flo)
-
 # TODO: PID (PSO) finden und Voicecoil
 # TODO: Voicecoil regeln und aufzeichnen
 
-# TODO: Stichworte zu BA, was solle diese beinhalten
-
 # TODO: Bode range dynamisch
-# TODO: Name von ClosedLoop in Plot anpassen
 
 # TODO: pdf generator: auch anregungsfunktion plotten
-# TODO: contional windup umsetzen oder rückbauen BA
 # TODO: Alternativer solver umsetzen
